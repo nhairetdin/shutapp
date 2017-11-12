f28c990a9be303809a4aab985b5a46a056dbd702
futstone <hairetdin.nico@gmail.com>
2017-11-12 12:18:10 +0200

    pages created

---
+++ src/pages/chat/chat.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-chat',
+  templateUrl: 'chat.html',
+})
+export class ChatPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ChatPage');
+  }
+
+}
